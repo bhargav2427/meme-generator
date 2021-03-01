8bba332426edb8ded7ca4a7ca9fd0897ef5f5bce
bhargav2427 <akhani.bharga@gmail.com>
2021-03-01 13:10:45 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,60 @@
+let topTextInput, bottomTextInput, imageInput, generateBtn, canvas, ctx;
+
+function generateMeme(img, topText, bottomText) {
+  canvas.width = img.width;
+  canvas.height = img.height;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(img, 0, 0);
+
+  let fontSize = canvas.width / 15;
+  ctx.font = "120pt Calibri";
+  //   ctx.font = fontSize + "px Impact";
+  ctx.fillStyle = "white";
+  ctx.strokeStyle = "black";
+  ctx.textAlign = "center";
+  ctx.lineWidth = 7;
+  ctx.textBaseline = "top";
+  topText.split("\n").forEach(function (t, i) {
+    ctx.strokeText(t, canvas.width / 2, i * fontSize, canvas.width);
+    ctx.fillText(t, canvas.width / 2, i * fontSize, canvas.width);
+  });
+
+  ctx.textBaseline = "bottom";
+  bottomText.split("\n").forEach(function (t, i) {
+    ctx.strokeText(
+      bottomText,
+      canvas.width / 2,
+      canvas.height - i * fontSize,
+      canvas.width
+    );
+    ctx.fillText(
+      bottomText,
+      canvas.width / 2,
+      canvas.height - i * fontSize,
+      canvas.width
+    );
+  });
+
+  //   ctx.strokeText(bottomText, canvas.width / 2, canvas.height, canvas.width);
+}
+
+function init() {
+  topTextInput = document.getElementById("top-text");
+  bottomTextInput = document.getElementById("bottom-text");
+  imageInput = document.getElementById("image-input");
+  generateBtn = document.getElementById("generate-btn");
+  canvas = document.getElementById("meme-canvas");
+  ctx = canvas.getContext("2d");
+  canvas.width = canvas.height = 0;
+  generateBtn.addEventListener("click", function () {
+    let reader = new FileReader();
+    reader.onload = function () {
+      let img = new Image();
+      img.src = reader.result;
+      generateMeme(img, topTextInput.value, bottomTextInput.value);
+    };
+    reader.readAsDataURL(imageInput.files[0]);
+  });
+}
+
+init();
